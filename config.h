cd41acc8dfbd8f1025ede696c9bbe6effbf3fd71
Jordan <jgeltchrobb@hotmail.com>
2022-07-28 22:28:52 +1000

    change dmenu font size

--- config.h
+++ config.h
@@ -4,15 +4,16 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=24"
+	"Source Code Pro:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+#include "/home/jordan/.cache/wal/colors-wal-dmenu.h"
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#bbbbbb", "#222222" },
+// 	[SchemeSel] = { "#eeeeee", "#005577" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
 
